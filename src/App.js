8aa3bccafa02605a2839baea0b68fd0f7b3312e9
Daviniel Nascimento <eng.daviniel@gmail.com>
2024-03-11 16:17:15 -0300

    adicionado a rota para a página do jogo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/index';
+import Game from './pages/Game';
 import PlayButton from './components/PlayButton';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
-      <Route path='/gamer' element={ <PlayButton /> }/>
+      <Route path='/game' element = { <Game /> } />
+      <Route path='/play' element={ <PlayButton /> }/>
     </Routes>
   );
 };
