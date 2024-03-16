53fd2955eb39105cdc942f991db55a9b4bebf1c7
Daviniel Nascimento <eng.daviniel@gmail.com>
2024-03-16 14:07:06 -0300

    corrigindo alguns erros

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setPlayerInfo } from '../../actions/Actions';
+import { setPlayerInfo } from '../../redux/actions/Actions';
 import  PlayButton  from '../../components/PlayButton';
 
 
-const Login = ({ player, setPlayerInfo }) => {
+const Login = () => {
+
+  this.state = {
+    playerName: '',
+    disabledButton: true
+  }
+
   const handleNameChange = (e) => {
     setPlayerInfo(e.target.value, player.email);
   };
+
+  componentDiMount() {
+    const { fetchApiTokenRedux, clearUserInformations } = this.props;
+    fetchApiTokenRedux();
+    clearUserInformations();
+  }
 
   const handleEmailChange = (e) => {
     setPlayerInfo(player.name, e.target.value);
   };
 
-  const isPlayButtonDisabled = !player.name.trim() || !player.email.trim();
-
+  validateEmailAndNameFilds = () => {
+    const { playerEmail, playerName } = this.state;
+    if (playerEmail.length !== 0 && playerName.length !== 0) {
+      this.setState({ disabledButton: false });
+    } else {
+      this.setState({ disabledButton: true });
+    }
+  }
+  
   return (
     <div>
       <label>Name:</label>
@@ -33,18 +52,26 @@ const Login = ({ player, setPlayerInfo }) => {
         data-testid="input-gravatar-email"
       />
 
-      <PlayButton disabled={isPlayButtonDisabled} />
+      <PlayButton 
+        buttonDisable={ disabledButton } 
+        onClickEvent = { () => {
+            playerActionRedux(playerName, playerEmail)
+          }
+          
+        }
+      
+      />
 
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
-  player: state.player,
+const mapDispatchToProps = (dispatch) => ({
+  fetchApiTokenRedux: () => dispatch(fetchApiToken()),
+  playerActionRedux: (playerName, playerEmail) => dispatch(
+    playerAction(playerName, playerEmail),
+  ),
+  clearUserInformations: () => dispatch(resetLoggedUserInformations()),
 });
 
-const mapDispatchToProps = {
-  setPlayerInfo,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(null, mapDispatchToProps)(Login);
