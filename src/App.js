a53215f8e9007a03526209b8df297464d4823a10
unknown <eng.daviniel@gmail.com>
2024-04-03 10:48:50 -0300

    adicionado a página de configuração

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/index';
 import Game from './pages/Game/index';
+import Settings from './pages/Settings/index';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
       <Route path='/game' element = { <Game /> } />
+      <Route path='/settings' element = { <Settings /> } />
     </Routes>
   );
 };
---
+++ src/pages/Settings/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function settings() {
+  return (
+    <h1 data-testid="settings-title">Page Seetings</h1>
+  );
+}
