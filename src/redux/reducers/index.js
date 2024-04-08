6bc5c19654533da9e2e264193a2595a48eae7171
unknown <eng.daviniel@gmail.com>
2024-04-08 09:58:00 -0300

    atualizando os reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,37 +1,11 @@
-// reducers/index.js
-import actions from '../actions/Actions';
+import { combineReducers } from 'redux';
+import player from './player';
+import token from './token';
 
-export const USER_EMAIL = 'USER_EMAIL';
-export const USER_LOGIN = 'USER_LOGIN';
+const rootReducer = combineReducers({
+  questionsReducer,
+  token,
+  player,
+});
 
-const initialState = {
-  player: {
-    name: '',
-    email: '',
-  },
-};
-
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case actions.USER_EMAIL:
-      return {
-        ...state,
-        player: {
-          ...state.player, 
-          email: action.payload,
-        },
-      };
-    case actions.USER_LOGIN:
-      return {
-        ...state,
-        player: {
-          ...state.player,
-          name: action.payload,
-        },
-      };
-    default:
-      return state;
-  }
-};
-
-export default rootReducer; // Remova { USER_EMAIL, USER_LOGIN } e adicione o ponto e vírgula aqui
+export default rootReducer;
---
+++ src/redux/reducers/player.js
@@ -0,0 +1,45 @@
+import {
+    CLEAR_USER_INFORMATIONS,
+    PLAYER_REDUCER,
+    SAVE_PLAYER_ASSERTIONS,
+    SAVE_POINTS,
+  } from '../actions';
+  
+  const INITIAL_STATE = {
+    name: '',
+    assertions: 0,
+    score: 0,
+    gravatarEmail: '',
+  };
+  
+  const player = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+    case PLAYER_REDUCER:
+      return {
+        ...state,
+        name: action.playerName,
+        gravatarEmail: action.playerEmail,
+      };
+    case SAVE_POINTS:
+      return {
+        ...state,
+        score: action.payload + state.score,
+      };
+    case SAVE_PLAYER_ASSERTIONS:
+      return {
+        ...state,
+        assertions: state.assertions + action.playerAssertions,
+      };
+    case CLEAR_USER_INFORMATIONS:
+      return {
+        name: '',
+        assertions: 0,
+        score: 0,
+        gravatarEmail: '',
+      };
+    default:
+      return state;
+    }
+  };
+  
+  export default player;
